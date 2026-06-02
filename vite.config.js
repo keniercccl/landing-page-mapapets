d89dae5437a8b2dd296814882fc41cb280279c6f
Kenier Becerra Becerra <kenier.becerra@siigroup.cl>
2026-06-02 19:47:49 -0400

    fix: configura para pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/landing-page-mapapets/',
+  base: 'https://keniercccl.github.io/landing-page-mapapets/',
   plugins: [react()],
 })
